6f288a1a85d31314d8e1893788b4cbbf41dcf070
pemba <pembas650@gmail.com>
2024-12-17 20:20:41 +0545

    footer added

---
+++ footer.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="./fontawesome-free-6.7.2-web/css/all.min.css">
+     <link rel="stylesheet" href="main.css">
+
+</head>
+<body>
+     <footer>
+        <div class="inner-footer">
+            <div class="card">
+                <h3>about us</h3>
+                <ul>
+                    <li>about us </li>
+                    <li>our difference</li>
+                    <li>community matters </li>
+                    <li>press</li>
+                    <li>blogs</li>
+                </ul>
+            </div>
+            <div class="card">
+                <h3>services</h3>
+                <ul>
+                    <li>orders </li>
+                    <li>term of use</li>
+                    <li>my account </li>
+                    <li>shipping</li>
+                    <li>account detail</li>
+                </ul>
+            </div>
+            <div class="card">
+                <h3>local</h3>
+                <ul>
+                    <li>bhaktapur</li>
+                    <li>kathmandu</li>
+                    <li>lalitpur </li>
+                    <li>ramechaap</li>
+                    <li>dolkha</li>
+                </ul>
+            </div>
+            <div class="card">
+                <h3>letter</h3>
+                <p>signup for latest offer</p>
+                <div class="input-field">
+                    <input type="text" name="" placeholder= "email address">
+                    <i class="fa-regular fa-envelope"></i>
+                </div>
+                <div class="social-links">
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-twitter"></i>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer" style = "display :flex ; justify-content : center ; align-items:center ; height : 50px" >
+            <p >all right reserved</p>
+        </div>
+     </footer>
+</body>
+</html>
